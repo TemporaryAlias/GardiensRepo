829bd1e2fd663d61860515ccd959dffa198e6f50
TemporaryAlias <38929684+TemporaryAlias@users.noreply.github.com>
2019-12-09 12:21:33 +0000

    Tutorial Proofread and Alarm Animation

---
+++ TestProjectAdherence/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public void CreateObject(GameObject objectToSpawn) {
+        Instantiate(objectToSpawn, gameObject.transform.parent);
+    }
+
+}
